2985a2efa7cd872340473660a8ac0da057ea904e
Dobzicle <669810@stud.hvl.no>
2024-03-13 12:18:46 +0100

    la til constructor og get/setters

--- src/main/java/bytebuddies/entities/Ansatt.java
+++ src/main/java/bytebuddies/entities/Ansatt.java
@@ -22,7 +22,107 @@ public class Ansatt {
     private float stillingsprosent;
     private String stillingstype;
 
+    public Ansatt(AnsattId ansattId, String hash, String salt, String fornavn, String etternavn, String telefonnummer, String epost, Adresse adresseId, boolean isActive, float stillingsprosent, String stillingstype) {
+        this.ansattId = ansattId;
+        this.hash = hash;
+        this.salt = salt;
+        this.fornavn = fornavn;
+        this.etternavn = etternavn;
+        this.telefonnummer = telefonnummer;
+        this.epost = epost;
+        this.adresseId = adresseId;
+        this.isActive = isActive;
+        this.stillingsprosent = stillingsprosent;
+        this.stillingstype = stillingstype;
+    }
 
     public Ansatt(){}
 
+    public AnsattId getAnsattId() {
+        return ansattId;
+    }
+
+    public void setAnsattId(AnsattId ansattId) {
+        this.ansattId = ansattId;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public String getEtternavn() {
+        return etternavn;
+    }
+
+    public void setEtternavn(String etternavn) {
+        this.etternavn = etternavn;
+    }
+
+    public String getTelefonnummer() {
+        return telefonnummer;
+    }
+
+    public void setTelefonnummer(String telefonnummer) {
+        this.telefonnummer = telefonnummer;
+    }
+
+    public String getEpost() {
+        return epost;
+    }
+
+    public void setEpost(String epost) {
+        this.epost = epost;
+    }
+
+    public Adresse getAdresseId() {
+        return adresseId;
+    }
+
+    public void setAdresseId(Adresse adresseId) {
+        this.adresseId = adresseId;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public float getStillingsprosent() {
+        return stillingsprosent;
+    }
+
+    public void setStillingsprosent(float stillingsprosent) {
+        this.stillingsprosent = stillingsprosent;
+    }
+
+    public String getStillingstype() {
+        return stillingstype;
+    }
+
+    public void setStillingstype(String stillingstype) {
+        this.stillingstype = stillingstype;
+    }
 }
