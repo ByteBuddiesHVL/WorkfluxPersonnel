ac1d5a533d6b12295873434433f28e42e4b07539
Henrik Taranger <hentik03@gmail.com>
2024-03-20 19:05:52 +0100

    fix

---
+++ src/main/resources/static/js/suite.js
@@ -0,0 +1,24 @@
+function setActive(button) {
+    let buttons = document.querySelectorAll('.button');
+        buttons.forEach(function (btn) {
+            btn.classList.remove('active');
+        })
+        button.classList.add('active');
+}
+
+const nyAnsatt = document.getElementById("nyAnsatt");
+const nyAnsattWindow = document.getElementById("nyAnsattWindow");
+const lag = document.getElementById("lag");
+
+nyAnsatt.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (nyAnsattWindow.style.display === "block") {
+        nyAnsattWindow.style.display = "none"
+    } else {
+        nyAnsattWindow.style.display = "block"
+    }
+})
+
+lag.addEventListener("click", () => {
+    nyAnsattWindow.style.display = "none";
+})
