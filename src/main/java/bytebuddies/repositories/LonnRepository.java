9df51349b928c02d7a37460cde90e2463020c980
danielsaga <669813@stud.hvl.no>
2024-03-24 07:54:48 +0100

    La til noen lønnsmetoder

--- src/main/java/bytebuddies/repositories/LonnRepository.java
+++ src/main/java/bytebuddies/repositories/LonnRepository.java
@@ -1,8 +1,15 @@
 package bytebuddies.repositories;
 
+import bytebuddies.entities.Ansatt;
 import bytebuddies.entities.Lonn;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface LonnRepository extends JpaRepository<Lonn, Integer> {}
+public interface LonnRepository extends JpaRepository<Lonn, Integer> {
+    Optional<Lonn> getTimelonnByAnsattId(Ansatt ansatt);
+
+    Optional<Lonn> getArslonnByAnsattId(Ansatt ansatt);
+}
